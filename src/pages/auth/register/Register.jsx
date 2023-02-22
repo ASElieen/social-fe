e3ef4ae93cbfe4dd3009bee3d0ea2cffeaf544c0
username <790768275@qq.com>
2023-02-22 18:46:03 +0800

    feat:(auth)注册页面中添加useState处理状态相关(样式，键入值)逻辑

--- src/pages/auth/register/Register.jsx
+++ src/pages/auth/register/Register.jsx
@@ -1,22 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '../../../components/input/Input';
 import Button from '../../../components/button/Button';
 import './Register.scss';
 
 const Register = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [alertType, setAlertType] = useState('');
+  const [hasError, setHasError] = useState(false);
+
+  const registerUser = async (e) => {
+    setLoading(true);
+    e.preventDefault();
+    try {
+      setAlertType('alert-success');
+      setHasError(false);
+    } catch (error) {
+      setLoading(false);
+      setHasError(true);
+      setAlertType('alert-error');
+      setErrorMessage(error?.response?.data.message);
+    }
+  };
   return (
     <div className="auth-inner">
-      <form className="auth-form">
+      {hasError && errorMessage && (
+        <div className={`alerts ${alertType}`} role={'alert'}>
+          {errorMessage}
+        </div>
+      )}
+      <form className="auth-form" onSubmit={registerUser}>
         <div className="form-input-container">
           {/* username */}
           <Input
             id="username"
             name="username"
             type="text"
-            value="text for now"
+            value={username}
             labelText="用户名"
-            placeholder="enter ur username"
-            handleChange={() => {}}
+            placeholder="请输入您的用户名"
+            handleChange={(e) => {
+              setUsername(e.target.value);
+            }}
           />
 
           {/* email field */}
@@ -24,10 +52,12 @@ const Register = () => {
             id="email"
             name="email"
             type="text"
-            value="test@text.com"
+            value={email}
             labelText="邮箱"
-            placeholder="enter ur Email"
-            handleChange={() => {}}
+            placeholder="请输入您的邮箱"
+            handleChange={(e) => {
+              setEmail(e.target.value);
+            }}
           />
 
           {/* password field */}
@@ -35,14 +65,20 @@ const Register = () => {
             id="password"
             name="password"
             type="text"
-            value="text for now"
+            value={password}
             labelText="密码"
-            placeholder="enter ur password"
-            handleChange={() => {}}
+            placeholder="请输入您的密码"
+            handleChange={(e) => {
+              setPassword(e.target.value);
+            }}
           />
         </div>
         {/* button component */}
-        <Button label={'注册'} className="auth-button button" disabled={false} />
+        <Button
+          label={`${loading ? '正在注册中,请稍后...' : '注册'}`}
+          className="auth-button button"
+          disabled={!username || !email || !password}
+        />
       </form>
     </div>
   );
