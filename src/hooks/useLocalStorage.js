d2450b94aedabbb9e3ec464a7c3ddb01d43c0644
username <790768275@qq.com>
2023-03-01 19:41:49 +0800

    feat:(hooks)添加localStorage和sessionStorage自定义hook

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,27 @@
+/**
+ * 
+ * @param {string} key localStorage key
+ * @param {string} type get set etc...
+ */
+const useLocalStorage = (key, type) => {
+    try {
+        if (type === 'get') {
+            const item = window.localStorage.getItem(key)
+            return item ? JSON.parse(item) : ''
+        } else if (type === 'set') {
+            const setValue = (newValue) => {
+                window.localStorage.setItem(key, JSON.stringify(newValue))
+            }
+            return [setValue]
+        } else {
+            const deleteValue = () => {
+                window.localStorage.removeItem(key)
+            }
+            return [deleteValue]
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export default useLocalStorage
